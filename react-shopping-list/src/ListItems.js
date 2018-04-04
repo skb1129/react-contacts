d60eeeb32e1f6aeb117a259d93271ca386a9b90b
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-04 16:05:41 +0530

    feat: use of stateless functional components

--- react-shopping-list/src/ListItems.js
+++ react-shopping-list/src/ListItems.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react';
 import { Table, Icon } from 'semantic-ui-react';
 
-class ListItems extends Component {
-  render() {
-    return(
-      <Table celled inverted selectable unstackable>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Quantity(Unit)</Table.HeaderCell>
-            <Table.HeaderCell>Unit Cost(₹)</Table.HeaderCell>
-            <Table.HeaderCell>Delete</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+function ListItems(props) {
+  return(
+    <Table celled inverted selectable unstackable>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Quantity(Unit)</Table.HeaderCell>
+          <Table.HeaderCell>Unit Cost(₹)</Table.HeaderCell>
+          <Table.HeaderCell>Delete</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
 
-        <Table.Body>
-          {this.props.items.map((item) => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{item.name}</Table.Cell>
-              <Table.Cell>{item.quantity}</Table.Cell>
-              <Table.Cell>{item.cost}</Table.Cell>
-              <Table.Cell textAlign='right'><Icon link name='delete' onClick={() => (this.props.removeItem(item))} /></Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    );
-  }
+      <Table.Body>
+        {props.items.map((item) => (
+          <Table.Row key={item.id}>
+            <Table.Cell>{item.name}</Table.Cell>
+            <Table.Cell>{item.quantity}</Table.Cell>
+            <Table.Cell>{item.cost}</Table.Cell>
+            <Table.Cell textAlign='right'><Icon link name='delete' onClick={() => (this.props.removeItem(item))} /></Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
 }
 
 export default ListItems;
