12a20280aa0c6da228dfcfb599e7494f4e1f8b58
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-04 14:14:25 +0530

    feat: list items component

--- react-shopping-list/src/ListItems.js
+++ react-shopping-list/src/ListItems.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import { Table, Icon } from 'semantic-ui-react';
+
 class ListItems extends Component {
   render() {
     return(
+      <Table celled inverted selectable unstackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell>Cost</Table.HeaderCell>
+            <Table.HeaderCell>Delete</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {this.props.items.map((item) => (
+            <Table.Row>
+              <Table.Cell>{item.name}</Table.Cell>
+              <Table.Cell>{item.quantity}</Table.Cell>
+              <Table.Cell>{item.cost}</Table.Cell>
+              <Table.Cell textAlign='right'><Icon link name='delete' /></Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     );
   }
 }
