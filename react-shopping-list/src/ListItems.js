bfe88f88cec2b8af0222c0b00045c6c1dc6b20f8
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-04 16:06:22 +0530

    Revert "feat: use of stateless functional components"
    This reverts commit d60eeeb32e1f6aeb117a259d93271ca386a9b90b.

--- react-shopping-list/src/ListItems.js
+++ react-shopping-list/src/ListItems.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import { Table, Icon } from 'semantic-ui-react';
 
-function ListItems(props) {
-  return(
-    <Table celled inverted selectable unstackable>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Name</Table.HeaderCell>
-          <Table.HeaderCell>Quantity(Unit)</Table.HeaderCell>
-          <Table.HeaderCell>Unit Cost(₹)</Table.HeaderCell>
-          <Table.HeaderCell>Delete</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-
-      <Table.Body>
-        {props.items.map((item) => (
-          <Table.Row key={item.id}>
-            <Table.Cell>{item.name}</Table.Cell>
-            <Table.Cell>{item.quantity}</Table.Cell>
-            <Table.Cell>{item.cost}</Table.Cell>
-            <Table.Cell textAlign='right'><Icon link name='delete' onClick={() => (this.props.removeItem(item))} /></Table.Cell>
+class ListItems extends Component {
+  render() {
+    return(
+      <Table celled inverted selectable unstackable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Quantity(Unit)</Table.HeaderCell>
+            <Table.HeaderCell>Unit Cost(₹)</Table.HeaderCell>
+            <Table.HeaderCell>Delete</Table.HeaderCell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
-  );
+        </Table.Header>
+
+        <Table.Body>
+          {this.props.items.map((item) => (
+            <Table.Row key={item.id}>
+              <Table.Cell>{item.name}</Table.Cell>
+              <Table.Cell>{item.quantity}</Table.Cell>
+              <Table.Cell>{item.cost}</Table.Cell>
+              <Table.Cell textAlign='right'><Icon link name='delete' onClick={() => (this.props.removeItem(item))} /></Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  }
 }
 
 export default ListItems;
