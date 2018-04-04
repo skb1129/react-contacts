7a775e0a474b8068247e1251c57150e2b7758929
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-04 17:33:27 +0530

    feat: use of controlled forms

--- react-shopping-list/src/AddItems.js
+++ react-shopping-list/src/AddItems.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { Form, Header, Button, Segment } from 'semantic-ui-react';
-import serializeForm from 'form-serialize';
 
 class AddItems extends Component {
+  state = {
+    name: '',
+    quantity: '',
+    cost: ''
+  }
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value })
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    const item = serializeForm(e.target, { hash: true });
     if (this.props.addItems)
-      this.props.addItems(item);
+      this.props.addItems(this.state);
+    this.setState({
+      name: '',
+      quantity: '',
+      cost: ''
+    });
   }
 
   render() {
@@ -16,11 +31,16 @@ class AddItems extends Component {
         <Form inverted onSubmit={this.handleSubmit}>
           <Header as='h3' inverted>Add New Items:</Header>
           <Form.Group widths='equal'>
-            <Form.Input label='Item Name' name='name' placeholder='Item Name' required />
-            <Form.Input label='Quantity' name='quantity' placeholder='Quantity' type='number' min={1} required />
-            <Form.Input label='Unit Cost' name='cost' placeholder='Unit Cost' type='number' min={0} required />
+            <Form.Input label='Item Name' name='name' placeholder='Item Name'
+              value={this.state.name} onChange={this.handleChange}/>
+            <Form.Input label='Quantity' name='quantity' placeholder='Quantity'
+              type='number' value={this.state.quantity}
+              onChange={this.handleChange}/>
+            <Form.Input label='Unit Cost' name='cost' placeholder='Unit Cost'
+              type='number' value={this.state.cost}
+              onChange={this.handleChange}/>
           </Form.Group>
-          <Button type='submit'>Submit</Button>
+          <Button type='submit' disabled={!this.state.name || !this.state.quantity || !this.state.cost}>Submit</Button>
         </Form>
       </Segment>
     );
