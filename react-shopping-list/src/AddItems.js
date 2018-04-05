1af2acf715582300da3adb4c6a94f49249712b7f
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-05 11:18:08 +0530

    refactor: item object in state

--- react-shopping-list/src/AddItems.js
+++ react-shopping-list/src/AddItems.js
@@ -8,38 +8,42 @@ class AddItems extends Component {
   }
 
   state = {
-    name: '',
-    quantity: '',
-    cost: ''
+    item: {
+      name: '',
+      quantity: '',
+      cost: ''
+    }
   }
 
   validateInputs = () => {
     return (
-      /^[a-zA-Z ]+$/.test(this.state.name) &&
-      /^[1-9][0-9]*$/.test(this.state.quantity) &&
-      /^[1-9][0-9]*$/.test(this.state.cost)
+      /^[a-zA-Z ]+$/.test(this.state.item.name) &&
+      /^[1-9][0-9]*$/.test(this.state.item.quantity) &&
+      /^[1-9][0-9]*$/.test(this.state.item.cost)
     )
   }
 
   handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    this.setState({ [name]: value })
+    let item = this.state.item;
+    item[name] = value;
+    this.setState({ item });
   };
 
   render() {
     return(
       <Segment inverted>
-        <Form inverted onSubmit={() => (this.props.addItems(this.state))}>
+        <Form inverted onSubmit={() => (this.props.addItems(this.state.item))}>
           <Header as='h3' inverted>Add New Items:</Header>
           <Form.Group widths='equal'>
             <Form.Input label='Item Name' name='name' placeholder='Item Name'
-              value={this.state.name} onChange={this.handleChange}/>
+              value={this.state.item.name} onChange={this.handleChange}/>
             <Form.Input label='Quantity' name='quantity' placeholder='Quantity'
-              type='number' value={this.state.quantity}
+              type='number' value={this.state.item.quantity}
               onChange={this.handleChange}/>
             <Form.Input label='Unit Cost' name='cost' placeholder='Unit Cost'
-              type='number' value={this.state.cost}
+              type='number' value={this.state.item.cost}
               onChange={this.handleChange}/>
           </Form.Group>
           <Message
@@ -54,6 +58,7 @@ class AddItems extends Component {
             disabled={!this.validateInputs()}>
             Submit</Button>
         </Form>
+        {JSON.stringify(this.state)}
       </Segment>
     );
   }
