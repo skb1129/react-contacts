7eb3caea13410a732fdee54c4fa2883cf743b3ee
Surya Kant Bansal <skb1129@yahoo.com>
2018-04-05 11:56:43 +0530

    feat: fixed form issues

--- react-shopping-list/src/AddItems.js
+++ react-shopping-list/src/AddItems.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Header, Button, Segment, Message } from 'semantic-ui-react';
+import { Form, Header, Button, Segment } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 class AddItems extends Component {
@@ -8,52 +8,50 @@ class AddItems extends Component {
   }
 
   state = {
-    item: {
-      name: '',
-      quantity: '',
-      cost: ''
-    }
+    name: '',
+    quantity: '',
+    cost: ''
   }
 
   validateInputs = () => {
     return (
-      /^[a-zA-Z ]+$/.test(this.state.item.name) &&
-      /^[1-9][0-9]*$/.test(this.state.item.quantity) &&
-      /^[1-9][0-9]*$/.test(this.state.item.cost)
+      /^[a-zA-Z ]+$/.test(this.state.name) &&
+      /^[1-9][0-9]*$/.test(this.state.quantity) &&
+      /^[1-9][0-9]*$/.test(this.state.cost)
     )
   }
 
   handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    let item = this.state.item;
-    item[name] = value;
-    this.setState({ item });
+    this.setState({ [name]: value });
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let item = {
+      name: this.state.name,
+      quantity: this.state.quantity,
+      cost: this.state.cost
+    };
+    this.props.addItems(item);
+  }
 
   render() {
     return(
       <Segment inverted>
-        <Form inverted onSubmit={() => (this.props.addItems(this.state.item))}>
+        <Form inverted onSubmit={this.handleSubmit}>
           <Header as='h3' inverted>Add New Items:</Header>
           <Form.Group widths='equal'>
             <Form.Input label='Item Name' name='name' placeholder='Item Name'
-              value={this.state.item.name} onChange={this.handleChange}/>
+              value={this.state.name} onChange={this.handleChange}/>
             <Form.Input label='Quantity' name='quantity' placeholder='Quantity'
-              type='number' value={this.state.item.quantity}
+              type='number' value={this.state.quantity}
               onChange={this.handleChange}/>
             <Form.Input label='Unit Cost' name='cost' placeholder='Unit Cost'
-              type='number' value={this.state.item.cost}
+              type='number' value={this.state.cost}
               onChange={this.handleChange}/>
           </Form.Group>
-          <Message
-            warning
-            header='Could you check something!'
-            list={[
-              'The item name must only contain alphabets',
-              'Quantity and Unit Cost must be a whole number'
-            ]}
-          />
           <Button type='submit'
             disabled={!this.validateInputs()}>
             Submit</Button>
